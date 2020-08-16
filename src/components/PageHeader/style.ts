db622bd7b3263b30958ca6b51e8f955f88de745d
Marcelo8173 <marceloandrebio@gmail.com>
2020-08-16 14:58:49 -0400

    initial refactoring of page header

---
+++ src/components/PageHeader/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: #FFFF;
+    height: inherit;
+`;
+
+export const ContentContainer = styled.div`
+    display: flex;
+`;
+
+export const Content = styled.section`
+
+`;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Container,BodyContainer } from './style';
 import Calendar from '../../components/Calendar';
 import AsideContent from '../../components/AsideContent';
+import PageHeader from '../../components/PageHeader';
 
 const Dashboard:React.FC = () =>{
     return(
         <Container>
-             
             <BodyContainer>
                 <Calendar />
-                <AsideContent />
+                <PageHeader />
             </BodyContainer>
         </Container>
     )
